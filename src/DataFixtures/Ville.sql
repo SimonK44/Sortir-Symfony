1b4fac728099d3b3d5d924b028c4ecdc6cc0d0d6
lboumendil2024 <lilian.boumendil2024@campus-eni.fr>
2024-08-28 08:58:08 +0200

    script ville

---
+++ src/DataFixtures/Ville.sql
@@ -0,0 +1,8 @@
+-- suppression enrgistrement table VILLES
+DELETE FROM villes;
+-- creation parametrage Ville
+INSERT INTO villes (nom_ville,code_postal) VALUES ('Nantes','44000');
+INSERT INTO villes (nom_ville,code_postal) VALUES ('Niort','79000');
+INSERT INTO villes (nom_ville,code_postal) VALUES ('Saint-Herblain','44850');
+INSERT INTO villes (nom_ville,code_postal) VALUES ('Rennes','35000');
+INSERT INTO villes (nom_ville,code_postal) VALUES ('Paris','75000');
