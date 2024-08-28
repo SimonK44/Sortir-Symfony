5cc541d274f327c7a2e8b0fcf1fd15bf5d8d0a4d
LilianBoumendil <lilian.boumendil2024@campus-eni.fr>
2024-08-28 16:52:35 +0200

    peinture ecrans sortie

--- src/DataFixtures/jeuxessai.sql
+++ src/DataFixtures/jeuxessai.sql
@@ -7,11 +7,8 @@ INSERT INTO `lieux` (`id`, `ville_id`, `nom_lieu`, `rue`, `latitude`, `longitude
 INSERT INTO `lieux` (`id`, `ville_id`, `nom_lieu`, `rue`, `latitude`, `longitude`) VALUES (NULL, '1', 'Karting', 'rue des chenes', '20.23', '1.21');
 
 -- Creation Sortie;
-INSERT INTO `sorties` (`id`, `organisateur_id`, `nom`, `date_debut`, `duree`, `date_cloture`, `nb_inscriptions_max`, `description_infos`, `etat_sortie`, `url_photo`, `etat_id`, `lieux_id`) VALUES (NULL, '1', 'cinema', '2024-08-28 08:14:31.000000', '1', '2024-08-28 08:14:31.000000', '10', 'sortie cinema', NULL, NULL, '12', '1');
-INSERT INTO `sorties` (`id`, `organisateur_id`, `nom`, `date_debut`, `duree`, `date_cloture`, `nb_inscriptions_max`, `description_infos`, `etat_sortie`, `url_photo`, `etat_id`, `lieux_id`) VALUES (NULL, '1', 'Karting', '2024-09-28 08:14:31.000000', '1', '2024-09-28 08:14:31.000000', '10', 'sortie karting', NULL, NULL, '12', '1');
-
-
-
+INSERT INTO `sorties` (`id`, `etat_id`, `lieux_id`, `user_id`, `site_id`, `nom`, `date_debut`, `duree`, `date_cloture`, `nb_inscriptions_max`, `description_infos`, `etat_sortie`, `url_photo`, `is_published`) VALUES (NULL, '1', '1', '1', '1', 'cinema', '2024-08-28 14:17:35.000000', '2', '2024-08-28 14:17:35.000000', '10', 'info', '1', NULL, '1');
+INSERT INTO `sorties` (`id`, `etat_id`, `lieux_id`, `user_id`, `site_id`, `nom`, `date_debut`, `duree`, `date_cloture`, `nb_inscriptions_max`, `description_infos`, `etat_sortie`, `url_photo`, `is_published`) VALUES (NULL, '1', '1', '1', '1', 'theatre', '2024-08-28 14:17:35.000000', '2', '2024-08-28 14:17:35.000000', '10', 'info', '1', NULL, '1');
 
 
 
